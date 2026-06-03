62001b73458fc787a49ab80daa1040354133d764
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-03 15:50:29 -0300

    alterações

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import '.style/css'
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import './assets/main.css' 
-
 const app = createApp(App)
 
 app.use(router)
