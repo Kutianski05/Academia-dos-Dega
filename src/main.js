3ae92a12fa78e287045f0b36db57cb2de8767e03
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-03 11:07:39 -0300

    teste de importação

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
+
+// ADICIONA ESTA LINHA EXATAMENTE AQUI:
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+// Se o teu projeto tiver um ficheiro de estilo global personalizado, ele costuma vir abaixo:
+import './assets/main.css' 
+
 const app = createApp(App)
 
 app.use(router)
-
 app.mount('#app')
