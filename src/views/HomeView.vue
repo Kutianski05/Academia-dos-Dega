e12d521f244657c090498e4096db4327416e2b4f
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-03 15:35:29 -0300

    Criação const planos e stats na homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,4 +7,34 @@ import PlanoCard from '../components/PlanoCard.vue'
 import ModalConfirmacao from '../components/ModalConfirmacao.vue'
 
 const router = useRouter()
+
+const planos = [
+    {
+        nome: 'Básico',
+        preco: 'R$ 120,00/mês',
+        icone: 'fas fa-star',
+        beneficios: ['Acesso a Musculação', 'Horários Livres', 'Vestiários']
+    },
+    {
+        nome: 'Intermediário',
+        preco: 'R$ 300,00/mês',
+        icone: 'fas fa-star-half-alt',
+        beneficios: ['Personal Trainer', 'Musculação', 'Aulas em grupo', 'App de Treino']
+    },
+    {
+        nome: 'Avançado',
+        preco: 'R$ 600,00/mês',
+        icone: 'fas fa-crown',
+        beneficios: ['Acompanhamento completo', 'Nutricionista', 'Personal Exclusivo', 'Suplementos']
+    }
+    ]
+
+const stats = [
+    {valor: 500, label: 'Alunos Ativos'},
+    {valor: 10, label: 'Personal Trainers'},
+    {valor: 1, label: 'Anos de Mercado'},
+    {valor: 200, label: 'Equipamentos'},
+]
+
+
 </script>
