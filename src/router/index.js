2689e8b8dd0a56a0c664fadb671b6cffaedd401a
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-03 15:47:43 -0300

    roteando a pagina HomeView

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/HomeView.vue'
 
+const routes = [
+  { path: '/', component: Home}
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
