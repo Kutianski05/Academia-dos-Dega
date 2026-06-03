12a7aaa9ca66746684be8bfc2d034a33fbdce09c
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-03 11:11:42 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from './router'
 
 // ADICIONA ESTA LINHA EXATAMENTE AQUI:
 import '@fortawesome/fontawesome-free/css/all.min.css'
-
-// Se o teu projeto tiver um ficheiro de estilo global personalizado, ele costuma vir abaixo:
 import './assets/main.css' 
 
 const app = createApp(App)
