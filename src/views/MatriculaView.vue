0da5c838196e49e81068ebde71caf68a3e7e908f
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-05 20:21:05 -0300

    const opcoesPlanos em MatriculaView

--- src/views/MatriculaView.vue
+++ src/views/MatriculaView.vue
@@ -4,4 +4,28 @@ import FooterBar from '../components/FooterBar.vue'
 import NavBar from '../components/NavBar.vue'
 import ModalConfirmacao from '@/components/ModalConfirmacao.vue'
 
+
+const opcoesPlanos = [
+    { valor: 'Basico', label: 'Básico -- 120,00 R$/ mês' },
+    { valor: 'Intermediario', label: 'Intermediário -- 300,00 R$/ mês' },
+    { valor: 'Avancado', label: 'Avançado -- 600,00 R$/ mês' },
+]
+
+const beneficios = [
+    'Equipamentos de última geração',
+    'Personal trainer certificados',
+    'Aulas coletivas incluídas',
+    'Estacionamento gratuito',
+    'App de acompanhamento',
+]
+
+const form = reactive({
+    nome: '', cpf: '', email: '', telefone: '', plano: ''
+})
+
+const erros = reactive({
+    
+})
+  
+
 </script>
