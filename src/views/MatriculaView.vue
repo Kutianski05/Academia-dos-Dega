c60e298faa80b32db8d4fb591799c1ff58534045
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-05 20:24:32 -0300

    const mascaraTelefone na MatriculaView

--- src/views/MatriculaView.vue
+++ src/views/MatriculaView.vue
@@ -38,6 +38,13 @@ const mascaraCPF = () => {
   else if (v.length > 3) v = v.replace(/(\d{3})(\d{0,3})/, '$1.$2')
   form.cpf = v
 }
+
+const mascaraTelefone = () => {
+    let v = form.telefone.replace(/\D/g, '')
+  if (v.length > 6) v = v.replace(/(\d{2})(\d{5})(\d{0,4})/, '$1 $2-$3')
+  else if (v.length > 2) v = v.replace(/(\d{2})(\d{0,5})/, '$1 $2')
+  form.telefone = v
+}
   
 
 </script>
