a86e5d6ddc81596a46385723eaa94a7daa810edf
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-05 17:44:19 -0300

    importação dos vue para a MatriculaView

---
+++ src/views/MatriculaView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { ref, reactive } from 'vue'
+import FooterBar from '../components/FooterBar.vue'
+import NavBar from '../components/NavBar.vue'
+import ModalConfirmacao from '@/components/ModalConfirmacao.vue'
+
+</script>
