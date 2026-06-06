75ab05c2d695ad02c533f03a7c70d863ef046099
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-05 21:15:39 -0300

    criação do caminho para a pagina matricula

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,9 @@ import Home from '../views/HomeView.vue'
 import Matricula from '../views/MatriculaView.vue'
 
 const routes = [
-  { path: '/', component: Home}
+  { path: '/', component: Home},
+  
+  {path: '/MatriculaView', component: Matricula}
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
