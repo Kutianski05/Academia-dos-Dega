bc39fbcea511d086fc31fbf1b7b1b4c592f29ded
Nelson <bolkotanelson@gmail.com>
2026-06-08 18:31:27 -0300

    view da Ficha de Treino com cabecalho e rodape

--- src/views/FichaTreinoView.vue
+++ src/views/FichaTreinoView.vue
@@ -1,3 +1,17 @@
 <template>
-    
+<div class="page">
+    <NavBar titulo="Ficha de Treino" />
+
+
+    <FooterBar texto="Página criada por Nelson" />
+</div>
 </template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+import NavBar from '../components/NavBar.vue'
+import FooterBar from '../components/FooterBar.vue'
+</script>
+
+<style scoped>
+</style>
