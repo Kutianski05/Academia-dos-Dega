c40c75615382313ae052dfaa10f9197f788ad94d
Nelson <bolkotanelson@gmail.com>
2026-06-08 18:33:27 -0300

    adiciona secao de exibicao dos dados do aluno

--- src/views/FichaTreinoView.vue
+++ src/views/FichaTreinoView.vue
@@ -2,6 +2,16 @@
 <div class="page">
     <NavBar titulo="Ficha de Treino" />
 
+    <div class="dados">
+      <div class="aluno">
+        <div class="avatar"><i class="fas fa-user-circle"></i></div>
+        <h2>Aluno: {{ aluno.nome }}</h2>
+      </div>
+      <br />
+      <p v-for="dado in dadosAluno" :key="dado.label">
+        <strong>{{ dado.label }}:</strong> {{ dado.valor }}
+      </p>
+    </div>
 
     <FooterBar texto="Página criada por Nelson" />
 </div>
@@ -11,6 +21,14 @@
 import { ref, reactive } from 'vue'
 import NavBar from '../components/NavBar.vue'
 import FooterBar from '../components/FooterBar.vue'
+
+const aluno = { nome: 'Dionatan Inhoato Markiu' }
+const dadosAluno = [
+  { label: 'Altura',   valor: '1,90 m' },
+  { label: 'Peso',     valor: '120 kg (seco)' },
+  { label: 'Objetivo', valor: 'Hipertrofia' },
+  { label: 'Tipo',     valor: 'CrossFit' },
+]
 </script>
 
 <style scoped>
