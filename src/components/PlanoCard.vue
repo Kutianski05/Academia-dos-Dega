f2de6338d180202ae979808b7e270a4b9cedfca6
Nelson <bolkotanelson@gmail.com>
2026-06-08 18:00:34 -0300

    adiciona layout, props e eventos no PlanoCard

--- src/components/PlanoCard.vue
+++ src/components/PlanoCard.vue
@@ -1,3 +1,18 @@
 <template>
-    
+    <div class="card" @click="$emit('selecionar', plano)">
+    <div class="icone"><i :class="plano.icone"></i></div>
+    <h3>{{ plano.nome }}</h3>
+    <p class="preco">{{ plano.preco }}</p>
+    <ul class="beneficios">
+      <li v-for="b in plano.beneficios" :key="b">
+        <i class="fas fa-check"></i> {{ b }}
+      </li>
+    </ul>
+    <button class="btn-plano">Escolher plano</button>
+  </div>
 </template>
+
+<script setup>
+defineProps({ plano: Object })
+defineEmits(['selecionar'])
+</script>
