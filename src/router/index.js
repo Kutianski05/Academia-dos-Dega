0c7badc6381e7340f853da738150a3ca9a54e0ab
Fabinhooo <engs-fabiofiilho@camporeal.edu.br>
2026-06-09 10:50:50 -0300

    Import Contato

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
 import Matricula from '../views/MatriculaView.vue'
+import ContatoView from '@/views/ContatoView.vue'
 
 const routes = [
   { path: '/', component: Home},
