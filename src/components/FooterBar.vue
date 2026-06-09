e1346e2fceb944e050fc1574b4a959c5026c2954
Fabinhooo <engs-fabiofiilho@camporeal.edu.br>
2026-06-09 09:55:15 -0300

    Template FooterBar

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="footer">
+        <div class="footer-content">
+            <p>{{ texto }}</p>
+            <div class="rede-social">
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-whatsapp"></i>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+defineProps(['texto'])
+</script>
+
