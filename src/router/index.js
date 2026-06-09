ac1072c4af31401bb81bbff82f3e66cd1e195519
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-06-09 12:12:32 -0300

    alterações

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
 import Matricula from '../views/MatriculaView.vue'
 import ContatoView from '@/views/ContatoView.vue'
+import FichaTreinoView from '@/views/FichaTreinoView.vue'
 
 const routes = [
   { path: '/', component: Home},
-  
-  {path: '/MatriculaView', component: Matricula}
+  {path: '/FichaTreinoView', component: Ficha-Treino},
+  {path: '/MatriculaView', component: Matricula},
+  {path: '/ContatoView', component: Contato}
 ]
 const router = createRouter({
   history: createWebHistory(),
